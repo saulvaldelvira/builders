e31cbe3c8901aa744913850dce5efe78c4062073
Saúl Valdelvira <saul@saulv.es>
2024-06-30 23:10:13 +0200

    Add IntoEnum derive

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ mod util;
 mod builder;
 
 use proc_macro::TokenStream;
+use proc_macro2::TokenTree;
+use syn::{Meta, MetaList};
 
 #[cfg(feature = "builder")]
 #[proc_macro_derive(Builder, attributes(builder))]
@@ -99,6 +101,62 @@ pub fn auto_box_derive(input: TokenStream) -> TokenStream {
         impl #generics #ident #stripped #wher {
             #vis fn as_box(self) -> std::boxed::Box<#ident #stripped> {
                 std::boxed::Box::new(self)
+            }
+        }
+    }.into()
+}
+
+#[cfg(feature = "into_enum")]
+#[proc_macro_derive(IntoEnum, attributes(into_enum))]
+pub fn wrap_enum_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse_macro_input!(input as syn::DeriveInput);
+    let ident = &ast.ident;
+    let generics = &ast.generics;
+    let stripped = util::get_stripped_generics(generics);
+    let wher = &generics.where_clause;
+
+    let mut enum_name = None;
+    let mut field_name = ident.clone();
+
+    for attr in ast.attrs {
+        if let Meta::List(MetaList { ref tokens, .. }) = attr.meta {
+            let mut tokens = tokens.clone().into_iter();
+
+            loop {
+                let Some(key) = tokens.next() else { break };
+                let TokenTree::Ident(ref name) = key else { panic!("Expected ident, found {key:#?}") };
+                match tokens.next().unwrap() {
+                    TokenTree::Punct(ref p) => assert_eq!(p.as_char(), '='),
+                    tt => panic!("Expected '=', found: {tt}"),
+                }
+                let val = tokens.next().unwrap();
+                let TokenTree::Ident(ref val) = val else { panic!("Expected ident, found {val:#?}") };
+
+                if name == "enum_name" {
+                    enum_name = Some(val.clone());
+                }
+                if name == "field" {
+                    field_name = val.clone();
+                }
+
+                if let Some(key) = tokens.next() {
+                    let TokenTree::Punct(ref name) = key else { panic!("Expected ',', found {key:#?}") };
+                    if name.as_char() == ',' {
+                        continue;
+                    } else {
+                        panic!()
+                    }
+                }
+            }
+        }
+    }
+
+    let enum_name = enum_name.unwrap();
+
+    quote::quote! {
+        impl #generics From<#ident #stripped> for #enum_name #wher {
+            fn from(value: #ident #stripped) -> Self {
+                #enum_name :: #field_name (value)
             }
         }
     }.into()
